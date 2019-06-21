f07f0da44de0e562ddb90b81791117ce1671475d
Laura Radville <laura.radville@gmail.com>
2019-06-21 13:22:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,10 @@ def monopoly_with_fourth_tier
 	      b_and_o_railroad: {
 	        "mortgage_value" => "$400"
 	      },
-	      shortline: {}
+	      shortline: {
+	        "mortgage_value" => "$800"
+
+	      }
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
